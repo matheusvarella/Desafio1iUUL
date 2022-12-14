417dcd00d93752e568d27e6fe7599972c33ae18b
Matheus Varella <varellamat@gmail.com>
2022-12-14 18:12:12 -0300

    Adicionar arquivos de projeto.

---
+++ AdmDentalOffice/ListAppointment.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdmDentalOffice
+{
+    public class ListAppointment
+    {
+
+        public Appointment findAppointmentForCpf(long cpf)
+        {
+            return new Appointment();
+        }
+    }    
+}
---
+++ AdmDentalOffice/Patient.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdmDentalOffice
+{
+    public class Patient
+    {
+        public string Name { get; set; }
+
+        public long Cpf { get; set; }
+
+        public DateTime BirthDate { get; set; }
+
+        public Patient(string name, long cpf, DateTime birthDate)
+        {
+            Name = name;
+            Cpf = cpf;
+            BirthDate = BirthDate;
+        }
+
+    }
+}
